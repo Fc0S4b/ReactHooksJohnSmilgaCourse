511a3929e0716846003d4a193290574677d303d7
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-25 18:07:02 -0400

    error example done

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title';
+  const handleClick = () => {
+    title = 'hello people';
+    console.log(title);
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>{' '}
+      {/*title no se actualiza al hacer click en el botón pese a que title cambia de valor, por esta razón usaremos useState */}
+      <button type="button" className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
