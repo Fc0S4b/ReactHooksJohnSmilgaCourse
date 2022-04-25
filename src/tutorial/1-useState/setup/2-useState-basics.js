ad9ebb4019a2de447a453bac4ccd21c5f7ad2775
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-25 18:23:54 -0400

    useState done

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
 import React, { useState } from 'react';
+import reactRouterDom from 'react-router-dom';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //console.log(useState())//[undefined, f] undefined es un valor default que se le pasa
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1];
+  // console.log(value, handler) //1 f
+  const [text, setText] = useState('random title'); //text y setText es convencional (apple setApple y así)
+  const handleClick = () => {
+    if(text === 'random title'){
+      setText('hello worold');
+
+    }
+    else{
+      setText('random title');
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
