13cb0fb9f621f2827f280ee4a4abb82553b52f43
Fc0S4b <fcoj.sabugo@gmail.com>
2022-04-29 22:42:27 -0400

    useState object done

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+  console.log(person);
+  const changeMessage = () => {
+    // setPerson('hello world'); al ejececutar la función se borran los h3, para poder cambiar solo una parte del objeto se puede usar el spread operator para así mantener a person y solo cambiar el message
+    setPerson({ ...person, message: 'hello world' });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
