3b40831d228ce07dd14f497c9ff75dfeffdd3704
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-13 19:39:19 -0400

    dependency list done

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -6,14 +6,17 @@ import React, { useState, useEffect } from 'react';
 // se puede solucionar usando un if dentro de useEffect
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
-  
+
   useEffect(() => {
     console.log('call useEffect');
-    if(value >= 1){
+    if (value >= 1) {
       document.title = `New Messages(${value}))`;
-      
     }
-  });
+  }, [value]);
+  // segundo parámetro si es [] entonces useEffect se ejecutará una vez cuando es renderizado pero si tiene un valor que cambia por ejemplo [value] entonces se ejecutará useEffect más de una vez a medida que cambie value. Este segundo argumento se llama lista de dependencias y activa el hook cada vez que la lista cambia
+  useEffect(() => {
+    console.log('hello world');
+  }, []);
   // console.log('render component') este se ve primero que call useEffect
   return (
     <>
