52a2b3f0c5b630deb059d8dcb8c041b21f9be55a
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-13 18:49:37 -0400

    useEffect conditional

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
-
+// no se puede usar hooks dentro de condicionales (como useEffect dentro de un if)
+// se puede solucionar usando un if dentro de useEffect
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+  
   useEffect(() => {
     console.log('call useEffect');
-    document.title = `New Messages(${value}))`;
+    if(value >= 1){
+      document.title = `New Messages(${value}))`;
+      
+    }
   });
   // console.log('render component') este se ve primero que call useEffect
   return (
