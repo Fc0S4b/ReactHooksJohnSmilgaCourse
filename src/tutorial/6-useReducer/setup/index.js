54d80d69f8cb66c2bcc6c43efca490ded6a09329
Fc0S4b <fcoj.sabugo@gmail.com>
2022-06-05 00:02:55 -0400

    index useReducer without useReducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -6,11 +6,15 @@ import { data } from '../../../data';
 const Index = () => {
   const [name, setName] = useState('');
   const [people, setPeople] = useState(data);
-  const [showModa, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      setShowModal(true);
+      setPeople([...people, { id: new Date().getTime().toString(), name }]);
+      setName('');
     } else {
+      setShowModal(true);
     }
   };
   return (
@@ -21,11 +25,18 @@ const Index = () => {
           <input
             type="text"
             value={name}
-            onChange={() => setName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <button type="submit">add</button>
       </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
     </>
   );
 };
